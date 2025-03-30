4a77ac5fde3b4ce5a079e8409ee0094af245bd59
JorgeGMarquez <jgonzalezmarquez@hawkmail.hccfl.edu>
2025-03-29 22:10:05 -0400

    completed next lesson and added function to make file into string and a function to count the words in the string

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_books_text(book_text):
+    got_book_text = book_text.read()
+    return(got_book_text)
+
+def count_words(book_text):
+    count = 0
+    book_string_list = book_text.split()
+    for word in book_string_list:
+        count += 1
+    return(count)
+
+book_text = None
+word_count = 0
+with open("books/frankenstein.txt") as text:
+    book_text = (get_books_text(text))
+    word_count = (count_words(book_text))
+    print(f"{word_count} words found in the document")
